84d7732bdccaf150b59eeb0718f6cc0573901ccf
eden <weiy3458@gmail.com>
2021-05-06 22:04:55 -0500

    updatec daemon task

--- application.go
+++ application.go
@@ -9,13 +9,16 @@ import (
 
 var App *Application
 
+type DaemonGo func()
+
 func init() {
 	App = NewApplication("./config/app.conf")
 }
 
 type Application struct {
-	router *Router
-	Config *Config
+	router     *Router
+	Config     *Config
+	daemonList []DaemonGo
 }
 
 func NewApplication(path string) *Application {
@@ -55,6 +58,9 @@ func (this *Application) Initialize() {
 func (this *Application) Run() {
 	host := this.Config.String("host")
 	http.Handle("/", this.router)
+	for i := 0; i < len(this.daemonList); i++ {
+		go this.daemonList[i]()
+	}
 	fmt.Println("server listen on " + host)
 	err := http.ListenAndServe(host, nil) //设置监听的端口
 	if err != nil {
@@ -66,6 +72,11 @@ func (this *Application) Add(prefix string, c ControllerInterface) *Application 
 	this.router.Add(prefix, c)
 	return this
 }
+
+func (this *Application) RegisterDeamonGo(daemonGo DaemonGo) {
+	this.daemonList = append(this.daemonList, daemonGo)
+}
+
 func (this *Application) AddStaticPath(prefix, path string) *Application {
 	this.router.AddStaticPath(prefix, path)
 	return this
