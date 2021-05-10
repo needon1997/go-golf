ce97e8eb1d451ce27189e041a135cfb5765bdde4
yidong <weiy3458@gmail.com>
2021-05-09 21:16:54 -0500

    add http client

--- golf_test.go
+++ golf_test.go
@@ -1,16 +1,87 @@
 package golf_test
 
 import (
-	"fmt"
-	"github.com/needon1997/go-golf"
+	"encoding/json"
+	"github.com/needon1997/go-golf/httplib"
+	"net/http"
 	"testing"
 )
 
-func TestName(t *testing.T) {
-	golf.App.RegisterDaemonGo(func() {
-		for i := 0; i < 10; i++ {
-			fmt.Println(i)
+//func TestDaemon(t *testing.T) {
+//	golf.App.RegisterDaemonGo(func() {
+//		for i := 0; i < 10; i++ {
+//			fmt.Println(i)
+//		}
+//	})
+//	golf.App.Run()
+//}
+
+func TestGet(t *testing.T) {
+	statusCode, _, err := httplib.Get("http://localhost:10081/echo")
+	if err != nil {
+		t.Error(err)
+	}
+	if statusCode != http.StatusOK {
+		t.Error("status not ok")
+	}
+}
+
+func TestPost(t *testing.T) {
+	reqBodyByte, err := json.Marshal(map[string]string{"msg": "hello"})
+	statusCode, rspBodyByte, err := httplib.Post("http://localhost:10081/echo", reqBodyByte)
+	if err != nil {
+		t.Error(err)
+	}
+	if statusCode != http.StatusOK {
+		t.Error("status not ok")
+	}
+	rspBody := make(map[string]string)
+	json.Unmarshal(rspBodyByte, &rspBody)
+	if msg, ok := rspBody["msg"]; !ok {
+		t.Error("wrong response")
+	} else {
+		if msg != "hello" {
+			t.Error("wrong response")
 		}
-	})
-	golf.App.Run()
+	}
+}
+
+func TestPut(t *testing.T) {
+	reqBodyByte, err := json.Marshal(map[string]string{"msg": "hello"})
+	statusCode, rspBodyByte, err := httplib.Put("http://localhost:10081/echo", reqBodyByte)
+	if err != nil {
+		t.Error(err)
+	}
+	if statusCode != http.StatusOK {
+		t.Error("status not ok")
+	}
+	rspBody := make(map[string]string)
+	json.Unmarshal(rspBodyByte, &rspBody)
+	if msg, ok := rspBody["msg"]; !ok {
+		t.Error("wrong response")
+	} else {
+		if msg != "hello" {
+			t.Error("wrong response")
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	reqBodyByte, err := json.Marshal(map[string]string{"msg": "hello"})
+	statusCode, rspBodyByte, err := httplib.Delete("http://localhost:10081/echo", reqBodyByte)
+	if err != nil {
+		t.Error(err)
+	}
+	if statusCode != http.StatusOK {
+		t.Error("status not ok")
+	}
+	rspBody := make(map[string]string)
+	json.Unmarshal(rspBodyByte, &rspBody)
+	if msg, ok := rspBody["msg"]; !ok {
+		t.Error("wrong response")
+	} else {
+		if msg != "hello" {
+			t.Error("wrong response")
+		}
+	}
 }
---
+++ httplib/httplib.go
@@ -0,0 +1,49 @@
+package httplib
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func Get(url string) (int, []byte, error) {
+	return parseResponse(makeRequest("GET", url, nil))
+}
+func Post(url string, requestBody []byte) (int, []byte, error) {
+	return parseResponse(makeRequest("POST", url, bytes.NewBuffer(requestBody)))
+}
+
+func Put(url string, requestBody []byte) (int, []byte, error) {
+	return parseResponse(makeRequest("PUT", url, bytes.NewBuffer(requestBody)))
+}
+
+func Delete(url string, requestBody []byte) (int, []byte, error) {
+	return parseResponse(makeRequest("DELETE", url, bytes.NewBuffer(requestBody)))
+}
+
+func makeRequest(method string, url string, requestBody io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, requestBody)
+	if err != nil {
+		return nil, err
+	}
+	response, err := http.DefaultClient.Do(req)
+	return response, err
+}
+func parseResponse(response *http.Response, err error) (int, []byte, error) {
+	if err != nil {
+		return -1, nil, err
+	}
+	body := response.Body
+	defer body.Close()
+	if response.StatusCode == http.StatusOK {
+		responseBody, err := ioutil.ReadAll(body)
+		if err != nil {
+			return response.StatusCode, nil, err
+		} else {
+			return response.StatusCode, responseBody, nil
+		}
+	} else {
+		return response.StatusCode, nil, nil
+	}
+}
